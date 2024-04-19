380fc84c434d01405aad761de76f5a432b87eecf
aman011210 <82104898+aman011210@users.noreply.github.com>
2024-04-19 15:47:48 +0530

    refractored client

---
+++ pocket_notes_app/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Notepage from "./pages/Notepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Notepage />
+    </div>
+  );
+}
+
+export default App;
